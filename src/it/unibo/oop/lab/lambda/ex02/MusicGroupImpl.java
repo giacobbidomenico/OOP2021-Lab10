74aa02927addc18e07e6c9014a6a17b40d8ec20a
Domenico Giacobbi <domenico.giacobbi@studio.unibo.it>
2021-12-05 11:51:47 +0100

    implementation of the method albumInYear in the class MusicGroupImpl

--- src/it/unibo/oop/lab/lambda/ex02/MusicGroupImpl.java
+++ src/it/unibo/oop/lab/lambda/ex02/MusicGroupImpl.java
@@ -43,7 +43,7 @@ public final class MusicGroupImpl implements MusicGroup {
 
     @Override
     public Stream<String> albumInYear(final int year) {
-        return null;
+        return this.albums.entrySet().stream().filter(e -> e.getValue() == year).map(e -> e.getKey());
     }
 
     @Override
