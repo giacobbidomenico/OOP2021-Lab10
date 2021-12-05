2a70d7990abf862ee96f6b82eb0d62d05fca93b2
Domenico Giacobbi <domenico.giacobbi@studio.unibo.it>
2021-12-05 20:56:18 +0100

    add javadoc documentation to the TestMusicGroup class

--- src/it/unibo/oop/lab/lambda/ex02/MusicGroupImpl.java
+++ src/it/unibo/oop/lab/lambda/ex02/MusicGroupImpl.java
@@ -10,18 +10,22 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- *
+ * Class representing a musical group.
  */
 public final class MusicGroupImpl implements MusicGroup {
 
     private final Map<String, Integer> albums = new HashMap<>();
     private final Set<Song> songs = new HashSet<>();
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void addAlbum(final String albumName, final int year) {
         this.albums.put(albumName, year);
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void addSong(final String songName, final Optional<String> albumName, final double duration) {
         if (albumName.isPresent() && !this.albums.containsKey(albumName.get())) {
@@ -29,22 +33,30 @@ public final class MusicGroupImpl implements MusicGroup {
         }
         this.songs.add(new MusicGroupImpl.Song(songName, albumName, duration));
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Stream<String> orderedSongNames() {
         return this.songs.stream().map(e -> e.getSongName()).sorted();
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Stream<String> albumNames() {
         return this.albums.keySet().stream();
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Stream<String> albumInYear(final int year) {
         return this.albums.entrySet().stream().filter(e -> e.getValue() == year).map(e -> e.getKey());
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int countSongs(final String albumName) {
         return (int) this.songs.stream()
@@ -52,14 +64,18 @@ public final class MusicGroupImpl implements MusicGroup {
                 .filter(song -> song.getAlbumName().get().equals(albumName))
                 .count();
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int countSongsInNoAlbum() {
         return (int) this.songs.stream()
                 .filter(song -> song.getAlbumName()
                 .isEmpty()).count();
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public OptionalDouble averageDurationOfSongs(final String albumName) {
         return this.songs.stream()
@@ -68,14 +84,18 @@ public final class MusicGroupImpl implements MusicGroup {
                 .mapToDouble(song -> song.getDuration())
                 .average();
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Optional<String> longestSong() {
         return this.songs.stream()
                 .max((x, y) -> Double.compare(x.getDuration(), y.getDuration()))
                 .map(song -> song.getSongName());
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Optional<String> longestAlbum() {
         return this.songs.stream()
@@ -85,33 +105,55 @@ public final class MusicGroupImpl implements MusicGroup {
                 .max((x, y) -> Double.compare(x.getValue(), y.getValue()))
                 .flatMap(x -> x.getKey());
     }
-
+    /**
+     * Class representing a song.
+     */
     private static final class Song {
 
         private final String songName;
         private final Optional<String> albumName;
         private final double duration;
         private int hash;
-
+        /**
+         * Builds a new {@link Song}.
+         * 
+         * @param name
+         * @param album
+         * @param len
+         */
         Song(final String name, final Optional<String> album, final double len) {
             super();
             this.songName = name;
             this.albumName = album;
             this.duration = len;
         }
-
+        /**
+         * Method that returns the name of the song.
+         * 
+         * @return the name of the song
+         */
         public String getSongName() {
             return songName;
         }
-
+        /**
+         * Method that returns the name of the album where is the song.
+         * 
+         * @return the name of the album where is the song
+         */
         public Optional<String> getAlbumName() {
             return albumName;
         }
-
+        /**
+         * Method that returns the duration of the song.
+         * 
+         * @return the duration of the song.
+         */
         public double getDuration() {
             return duration;
         }
-
+        /**
+         * {@inheritDoc}
+         */
         @Override
         public int hashCode() {
             if (hash == 0) {
@@ -119,7 +161,9 @@ public final class MusicGroupImpl implements MusicGroup {
             }
             return hash;
         }
-
+        /**
+         * {@inheritDoc}
+         */
         @Override
         public boolean equals(final Object obj) {
             if (obj instanceof Song) {
@@ -129,7 +173,9 @@ public final class MusicGroupImpl implements MusicGroup {
             }
             return false;
         }
-
+        /**
+         * {@inheritDoc}
+         */
         @Override
         public String toString() {
             return "Song [songName=" + songName + ", albumName=" + albumName + ", duration=" + duration + "]";
